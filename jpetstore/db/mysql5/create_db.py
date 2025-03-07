0494ad87a189f769ed9d675f53068e82e41b467d
Carlos Ricardo Rodriguez Arroyo <Carlos.Arroyo@ocean.ibm.com>
2025-03-07 13:27:03 -0600

    fix: revert changes in create_db.py file

--- jpetstore/db/mysql5/create_db.py
+++ jpetstore/db/mysql5/create_db.py
@@ -1,14 +1,11 @@
 import mysql.connector
 from mysql.connector import errorcode
-from mysql.connector import MySQLConnection
 import os
-#url = os.environ.get('mysql.url')
-#user = os.environ.get('mysql.username')
-#pswd = os.environ.get('mysql.password')
-url = "mysqldb-45573c988f669ce564cd638aec565c29.mysql.database.azure.com"
-user = "db_admin"
-pswd = "Passw0rd1!"
+import codecs
 
+url = os.environ.get('mysqlUrl')
+user = os.environ.get('mysqlUser')
+pswd = os.environ.get('mysqlPass')
 # Obtain connection string information from the portal
 config = {
   'host': url,
@@ -16,25 +13,20 @@ config = {
   'password': pswd,
   'database':'mysql'
 }
-print(config)
 # Construct connection string
 try:
-   conn = MySQLConnection(**config)
+   conn = mysql.connector.connect(**config)
+   cursor = conn.cursor()
    print("Connection established")
-   exit()
-except Exception as err:
-  print(err)
-  exit()
+except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with the user name or password")
   elif err.errno == errorcode.ER_BAD_DB_ERROR:
     print("Database does not exist")
   else:
     print(err)
-else:
-    cursor = conn.cursor()
-import codecs
-#fd = codecs.open('/src/jpetstore-mysql-1-schema.sql', mode='r', encoding='utf-8-sig', errors='ignore')
+
+fd = codecs.open('/src/jpetstore-mysql-1-schema.sql', mode='r', encoding='utf-8-sig', errors='ignore')
 operation = fd.read()
 fd.close()
 try:
@@ -49,7 +41,7 @@ try:
 except:
   print("###")
 
-#fd = codecs.open('/src/jpetstore-mysql-2-dataload.sql', mode='r', encoding='utf-8-sig', errors='ignore')
+fd = codecs.open('/src/jpetstore-mysql-2-dataload.sql', mode='r', encoding='utf-8-sig', errors='ignore')
 operation = fd.read()
 fd.close()
 try:

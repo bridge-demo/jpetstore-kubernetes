d067a0c86bbeb45d53c5a59f4fa5d5df975b8104
Carlos Ricardo Rodriguez Arroyo <Carlos.Arroyo@ocean.ibm.com>
2025-04-03 10:28:32 -0600

    fix: fix get order details

--- pipeline-python/emp_order_reader.py
+++ pipeline-python/emp_order_reader.py
@@ -175,7 +175,8 @@ def get_order_details_for_service_chaining(tenant_user_id, tenant_system_user_ap
     tenantApiUrl = common_utils.sanitazeTenantUrl(tenant_api_url)
     bearerToken = common_utils.get_bearer_token(tenantUrl=tenantApiUrl, apikey=tenant_system_user_api_key, subject=tenant_user_id)
     tenantApiUrl = common_utils.sanitazeTenantUrl(tenant_api_url, "api")
-    ENDPOINT = f"{tenantApiUrl}consume/v5/api/orders/{order_number}/detail"
+    ENDPOINT = f"{tenantApiUrl}/v5/api/orders/{order_number}/detail"
+    # PROD ENDPOINT = f"{tenantApiUrl}consume/v5/api/orders/{order_number}/detail"
     headers = {
         # "username": tenant_user_id, 
         # "apikey": tenant_system_user_api_key

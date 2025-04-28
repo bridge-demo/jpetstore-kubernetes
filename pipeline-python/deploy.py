c38e2946db35b4e689634b0fabd86b2fa2fe5999
Carlos Ricardo Rodriguez Arroyo <Carlos.Arroyo@ocean.ibm.com>
2025-04-28 16:03:47 -0600

    PetstorePattern Prod

--- pipeline-python/deploy.py
+++ pipeline-python/deploy.py
@@ -121,7 +121,7 @@ class Deploy:
             LOGGER.error(result.stderr)
             raise Exception( result.args )
         
-        installNgnix = f"helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --kubeconfig tmp_kube_config --debug".split(" ")
+        installNgnix = f"helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --wait --kubeconfig tmp_kube_config".split(" ")
         result = subprocess.run( installNgnix )
         if result.returncode != 0:
             LOGGER.error(f"Fail to install ingress-nginx")

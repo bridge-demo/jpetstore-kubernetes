17119e440fca52e0b29c08adced9ba263c2703b2
Carlos Ricardo Rodriguez Arroyo <Carlos.Arroyo@ocean.ibm.com>
2025-05-19 12:41:31 -0600

    PetstorePattern Stand Alone

--- pipeline-python/common_utils.py
+++ pipeline-python/common_utils.py
@@ -74,7 +74,7 @@ def sanitazeTenantUrl(tenantUrl:str, urlType:str ="url"):
 
 
 def file_exists(path:str)-> bool:
-    fileExists = os.path.exists("./tmp_kube_config.yaml")
+    fileExists = os.path.exists("./pipeline-python/tmp_kube_config.yaml")
     if not fileExists:
         print(f"Error: Kube config ('tmp_kube_config') not found in local path")
         localFiles = subprocess.getoutput("cd ./pipeline-python/; ls")
